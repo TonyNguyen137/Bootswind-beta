86e6230b0c9bdb364e07b26eb52888bb19d51dc4
Tony Nguyen <tonngu137@gmail.com>
2025-10-17 20:45:58 +0200

    adjusted Backdrop

--- js/modules/navbar-offcanvas/Backdrop.js
+++ js/modules/navbar-offcanvas/Backdrop.js
@@ -1,17 +1,18 @@
-import { Utils } from '../../utils';
-
 export class Backdrop {
   constructor(context) {
     this._offcanvasEl = context.offcanvasEl;
     this._containerEl = context.containerEl;
-    this._backdropEl = document.createElement('div');
-    this._backdropEl.classList.add('backdrop', 'fade');
+    this._backdropEl = document.createElement("div");
+    this._backdropEl.classList.add("backdrop", "fade");
 
     this._initEventListener();
   }
 
   _initEventListener() {
-    this._backdropEl.addEventListener('transitionend', this._handleTransitionEnd.bind(this));
+    this._backdropEl.addEventListener(
+      "transitionend",
+      this._handleTransitionEnd.bind(this)
+    );
   }
 
   onExpand() {
@@ -21,17 +22,17 @@ export class Backdrop {
   onOpen() {
     this._containerEl.append(this._backdropEl);
     requestAnimationFrame(() => {
-      this._backdropEl.classList.add('show');
+      this._backdropEl.classList.add("show");
     });
   }
 
   onClose() {
-    this._backdropEl.classList.remove('show');
+    this._backdropEl.classList.remove("show");
   }
 
   _handleTransitionEnd(e) {
     // once the the fade-out is complete, remove the backdrop from the DOM
-    if (!e.target.classList.contains('show')) {
+    if (!e.target.classList.contains("show")) {
       // Remove BackdropEl from the DOM
       this._backdropEl.remove();
     }
